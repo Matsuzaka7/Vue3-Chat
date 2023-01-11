dce1b4ba962e45794e855025b2620c5a7227c4c9
松坂砂糖 <83869273+Matsuzaka7@users.noreply.github.com>
2023-01-12 00:31:19 +0800

    Update index.js

--- end/config/index.js
+++ end/config/index.js
@@ -1,7 +1,7 @@
 import path from 'path'
 
-export const wsPort = 1001
-export const httpPort = 1000
+export const wsHost = 1001
+export const httpHost = 1000
 // 聊天数据路径
 export const infoDataPath = path.resolve('data/infoData.json')
 // 用户ip数据路径
